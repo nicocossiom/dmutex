126662c0639ed17aeb71ed9a90ab5002b6994b28
nicocossio <nicocossiom@gmail.com>
2022-05-21 14:04:22 +0200

    socket done

--- Proceso-src/main.c
+++ Proceso-src/main.c
@@ -7,15 +7,15 @@
  *      de las mismas en diferentes ficheros.
  */
 
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 
-int puerto_udp;
+int udp_port;
 
-int main(int argc, char* argv[]) {
-    int port;
+int main(int argc, char *argv[]) {
     char line[80], proc[80];
 
     if (argc < 2) {
@@ -24,20 +24,40 @@ int main(int argc, char* argv[]) {
     }
 
     /* Establece el modo buffer de entrada/salida a linea */
-    setvbuf(stdout, (char*)malloc(sizeof(char) * 80), _IOLBF, 80);
-    setvbuf(stdin, (char*)malloc(sizeof(char) * 80), _IOLBF, 80);
-    /* Se determina el puerto UDP que corresponda.
-      Dicho puerto debe estar libre y quedara
-      reservado por dicho proceso. */
-    puerto_udp = 1111;
+    setvbuf(stdout, (char *)malloc(sizeof(char) * 80), _IOLBF, 80);
+    setvbuf(stdin, (char *)malloc(sizeof(char) * 80), _IOLBF, 80);
+    // Socket creation
+    struct sockaddr_in process_addr;
+    int process_fd;
+    if ((process_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    // socket address configuration
+    process_addr.sin_family = AF_INET;
+    process_addr.sin_addr.s_addr = INADDR_ANY;
+    process_addr.sin_port = 0;
+    // socket binding to random available port
+    if (bind(process_fd, (struct sockaddr *)&process_addr, sizeof(process_addr)) < 0) {
+        perror("error in process address binding");
+        close(process_fd);
+        exit(EXIT_FAILURE);
+    }
+    // get the port which the socket is binded to
+    socklen_t len = sizeof(process_addr);
+    if (getsockname(process_fd, (struct sockaddr *)&process_addr, &len) == -1) {
+        perror("getsockname");
+        close(process_fd);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "%s: %d\n", argv[1], ntohs(process_addr.sin_port));
 
-    fprintf(stdout, "%s: %d\n", argv[1], puerto_udp);
 
     for (; fgets(line, 80, stdin);) {
         if (!strcmp(line, "START\n"))
             break;
 
-        sscanf(line, "%[^:]: %d", proc, &port);
+        sscanf(line, "%[^:]: %d", proc, &udp_port);
         /* Habra que guardarlo en algun sitio */
 
         if (!strcmp(proc, argv[1])) { /* Este proceso soy yo */
